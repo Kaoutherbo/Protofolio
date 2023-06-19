b715b55105564098d3bee741357e6f7039fe034c
kaouther <kaoutherboutheldja@gmail.com>
2023-06-19 22:33:07 +0100

    "Update Size"

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import Skills from "./components/Skills/Skills";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import'./index.css';
+// Particles
+import ParticlesComponent from './components/Particle';
 export default function App() {
   return (
     <main> 
-   
+      <ParticlesComponent />
       <Home />
       <About />
       <Projects />
